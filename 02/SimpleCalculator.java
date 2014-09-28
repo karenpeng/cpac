8ecbac3dde116c0bbff9bd4e60c4bb18c7a3aae8
Karen <karenpenglabs@gmail.com>
2014-09-28 00:52:06 -0400

    everything before using regular expression

--- 02/SimpleCalculator.java
+++ 02/SimpleCalculator.java
@@ -6,7 +6,6 @@
 **/
 
 import java.util.Scanner;
-import java.util.*;
 
 public class SimpleCalculator {
 
@@ -30,12 +29,83 @@ public class SimpleCalculator {
     System.out.println(result);
 
     askForNumber();
-    System.out.println(result);
+    System.out.println("The result is "+result);
 
     askForSomething();
   }
 
-  public static boolean isDigit(char c){
+  public static void askForNumber(){
+    float f = 0.f;
+    System.out.println("Give me a number:");
+    String input = reader.next();
+
+    if(input.length() == 1){
+      char c = input.charAt(0);
+      if(isSpecialCommand(c)){
+
+      }else if(isDigit(c)){
+        f = Float.parseFloat(input);
+      }else{
+        System.out.println("Unknown number "+ c);
+        System.out.print("Try again. ");
+        askForNumber();
+      }
+
+    }else{
+      int pointCount = 0;
+      for(int i = 0; i < input.length(); i++){
+        if(i == 0 && input.charAt(i) == '-'){
+          i++;
+        }
+        char c = input.charAt(i);
+        if(c == '.'){
+          pointCount ++;
+        }
+        if((!isDigit(c) && c != '.') || pointCount > 1){
+          System.out.println("Unknown number "+ c);
+          System.out.print("Try again. ");
+          askForNumber();
+        }
+      }
+      f = Float.parseFloat(input);
+    }
+
+      if(mode == 'n' || mode =='+') {result += f;}
+      if(mode == '-') {result -= f;}
+      if(mode == '*') {result *= f;}
+      if(mode == '/') {
+        if(f == 0.f){
+          System.out.println("Division by zero.");
+        }else {
+          result /= f;
+        }
+      }
+
+  }
+
+  public static void askForOperator(){
+    System.out.println("Give me an operator:");
+    String input = reader.next();
+
+    if(input.length() == 1){
+      char c = input.charAt(0);
+      if(isSpecialCommand(c)){
+
+      }else if(isOperator(c)){
+        mode = c;
+      }else{
+      System.out.println("Unknown operator "+ input);
+      System.out.print("Try again, ");
+      askForOperator();
+     }
+    }else{
+      System.out.println("Unknown operator "+ input);
+      System.out.print("Try again, ");
+      askForOperator();
+    }
+  }
+
+    public static boolean isDigit(char c){
     if(c == '0' || c == '1'|| c == '2' || c == '3' || c == '4' || c == '5'|| c == '6' || c == '7' || c == '8' || c == '9' ){
       return true;
     }else{
@@ -51,62 +121,15 @@ public class SimpleCalculator {
     }
   }
 
-  public static void askForNumber(){
-    float f = 0.f;
-    System.out.println("Give me a number:");
-    String input = reader.next();
-
-    if(input.length() == 1){
-      char c = input.charAt(0);
-      if(c == 'x'){
-        System.exit(0);
-      }else if(c == 'c'){
-        result = 0.f;
-      }else if(isDigit(c)){
-        f = Float.parseFloat(input);
-      }else{
-        System.out.println("Unknown number "+ c);
-        System.out.print("Try again, ");
-        askForNumber();
-      }
-
+  public static boolean isSpecialCommand(char c){
+    if(c == 'c'){
+     System.exit(0);
+     return true;
+    }else if(c == 'x'){
+      result = 0.f;
+      return true;
     }else{
-      for(int i = 0; i < input.length(); i++){
-        if(i == 0 && input.charAt(i) == '-'){
-          i++;
-        }
-        char c = input.charAt(i);
-        if(!isDigit(c) && c != '.'){
-          System.out.println("Unknown number "+ c);
-          System.out.print("Try again, ");
-          askForNumber();
-        }
-      }
-      f = Float.parseFloat(input);
-    }
-
-      if(mode == 'n' || mode =='+') {result += f;}
-      if(mode == '-') {result -= f;}
-      if(mode == '*') {result *= f;}
-      if(mode == '/') {
-        if(f == 0.f){
-          System.out.println("division by zero");
-        }else {
-          result /= f;
-        }
-      }
-
-  }
-
-  public static void askForOperator(){
-    System.out.println("Give me an operator:");
-    String input = reader.next();
-    if(input.length() == 1 && isOperator(input.charAt(0))){
-      mode = input.charAt(0);
-    }else {
-      System.out.println("Unknown operator "+ input);
-      System.out.print("Try again, ");
-      askForOperator();
+      return false;
     }
   }
 
