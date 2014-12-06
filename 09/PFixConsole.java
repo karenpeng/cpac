5fc033c102541a54e2f04e9669733b28e46bda8b
Karen <karenpenglabs@gmail.com>
2014-12-06 17:26:32 -0500

    help

---
+++ 09/PFixConsole.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PFixConsole{
+
+  Scanner reader = new Scanner(System.in);
+
+  public PFixConsole{
+
+  }
+
+  public void main(){
+    //open a console?
+    System.out.println("give me something");
+    String input = reader.next();
+
+  }
+}
---
+++ 09/PostFixEvaluator.java
@@ -0,0 +1,13 @@
+public class PostFixEvaluator{
+
+  public PostFixEvaluator{
+
+  }
+
+  public double evaluation(String postfixExpressiong){
+    double result = 0.0;
+    return result;
+  }
+
+
+}
