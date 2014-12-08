dcc9e0a2970789ee1ad1088b7f7d266edc30cfc8
Karen <karenpenglabs@gmail.com>
2014-12-08 12:06:53 -0500

    something wrong with the converter

--- 09/Converter.java
+++ 09/Converter.java
@@ -3,34 +3,132 @@ import java.util.ArrayList;
 public class Converter{
 
   ArrayList<String> input = new ArrayList<String>();
-  ArrayList<String> stack = new ArrayList<String>();
-  ArrayList<String> output = new ArrayList<String>();
+  ArrayList<Token> stack = new ArrayList<Token>();
+  ArrayList<Token> output = new ArrayList<Token>();
 
   //i think the stack goes here
-  public ArrayList<String> toPostFix(ArrayList<String> infixExpression){
+  public String toPostFix(ArrayList<String> infixExpression){
 
     input = infixExpression;
-    ArrayList<String> postfixExpression = new ArrayList<String>();
+    ArrayList<Token> tokens = new ArrayList<Token>();
+    String postfixExpression = "";
 
-    for(int i = 0; i < input.size(); i++){
-      String s = input.get(i);
-      char c = s.toCharArray()[0];
-      if(Character.isDigit(c)){
-        output.add(s);
-      }else if(c == ')'){
-        stack.remove('(');
-        stack.put into output
-      }else{
-        stack.add(s);
-      }
-
-      postfixExpression.add(infixExpression.get(i));
+    for(String s : infixExpression){
+      tokens.add(new Token(s));
     }
 
+    for(Token t : tokens){
+
+      //if it is a number, simply put it in the output
+      if(t.operand){
+        output.add(t);
+
+      }else{
+
+        //Whenever you come upon an open parenthesis, always put it on the stack
+        if(t.str == "("){
+          stack.add(t);
+        }
+
+        //Whenever you come upon a closed parenthesis,
+        //pop out all the operators on the stack and append them to output until you find the matching parenthesis.
+        //Pop out the matching parenthesis and don't add either paren to the output
+        //(remember: postfix doesn't have parenthesis!)
+        else if(t.str == ")"){
+
+          for(int i = stack.size()-1; i > 0; i-- ){
+            Token onTopOfStack = stack.get(i);
+            if(onTopOfStack.str != "("){
+              stack.remove(onTopOfStack);
+              output.add(onTopOfStack);
+            }else{
+              stack.remove(onTopOfStack);
+              break;
+            }
+          }
+
+        //let's deal with operators
+        }else{
+
+          //if the stack is empty
+          if(stack.size() == 0){
+            stack.add(t);
+
+          }else{
+
+            //if there is an open parenthesis in the stack
+            Token onTopOfStack = stack.get(stack.size()-1);
+            if(onTopOfStack.str == "("){
+              stack.add(t);
+            }
+
+           //if operator on the stack has a lower precedence, append the one you are reading to the stack
+            else if(t.precedence > onTopOfStack.precedence){
+              stack.add(t);
+            }else{
+
+              //pop operators out of the stack and append to output,
+              //until the operator at the top of the stack is of lower precedence than the token you are reading
+              for(int i = stack.size()-1; i > 0; i-- ){
+                onTopOfStack = stack.get(i);
+                if(t.precedence <= onTopOfStack.precedence){
+                  stack.remove(onTopOfStack);
+                  output.add(onTopOfStack);
+                }else{
+                  stack.add(onTopOfStack);
+                  break;
+                }
+              }
+
+            }
+
+          }
+        }
+
+      }
+    }
+
+
+    for(Token t : output){
+      postfixExpression += (t.str + " ");
+    }
+    System.out.println("postfixExpression "+ postfixExpression);
     return postfixExpression;
+
   }
 
-  public boolean isParenthesis(char c){
+  public class Token{
+
+    String str = "";
+    boolean operator =  false;
+    boolean operand = false;
+    int precedence = 0;
+    boolean parenthesis = false;
+
+    Token(String s){
+      str = s;
+      tokenize(s.toCharArray()[0]);
+    }
+
+    void tokenize(char c){
+      if(Character.isDigit(c)){
+        operand = true;
+      }else{
+
+        if(c == '(' || c == ')'){
+          parenthesis = true;
+          return;
+        }
+
+        if(c == '*' || c == '/' || c == '^'){
+          precedence = 2;
+        }else if( c == '+' || c == '-' ){
+          precedence = 1;
+        }
+        operator = true;
+      }
+
+    }
 
   }
 
