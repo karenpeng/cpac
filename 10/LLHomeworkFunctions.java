00f5c1d009b31b367361c57d8e6d5f03550d3f6d
Karen <karenpenglabs@gmail.com>
2014-12-12 23:55:14 -0500

    linked list and recursion :)

---
+++ 10/LLHomeworkFunctions.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class LLHomeworkFunctions {
+
+	public static void main(String [] args) {
+        // TODO: Part of this assignment is to implement this correctly.
+	}
+
+	/*
+	 * @param <T>
+	 * @param list1
+	 * @param list2
+	 * @return true if the lists are equal.  Assume both lists terminate.
+	 */
+
+	//the big O for this is n
+    public static <T> boolean equalLists(ListNode<T> list1, ListNode<T> list2) {
+		// TODO: Part of this assignment is to implement this correctly.
+			if(list1.value.equals(list2.value)){
+				//null is for address, so use '==' instead of equals
+				if(list1.value == null){
+					return true;
+				}else{
+					equalLists(list1.next, list2.next);
+				}
+			}else return false;
+
+	}
+
+
+	/*
+	 * @param <T>
+	 * @param list1
+	 * @return true if the list eventually terminates, and false if the list points back at one of it's
+	 *  previous nodes.
+	 */
+
+	//the big O for this is n^2
+	ArrayList<T> values = new ArrayList<T>();
+	public static <T> boolean terminates(ListNode<T> list) {
+        // TODO: Part of this assignment is to implement this correctly.
+
+        // code below is never going to end, forget it
+       	//  if(list == null){
+       	//  	return true;
+	      // }else{
+	      // 	terminates(list.next);
+	      // }
+
+	      //well, checking value
+	      if(list.value == null) return true;
+
+	      for(T t: values){
+	      	if(t.equals(list.value){
+	      		return false;
+	      	}
+	      }
+    		values.add(list.value);
+    		terminates(list.next);
+
+    		//code below uses hashTable
+
+	}
+}
